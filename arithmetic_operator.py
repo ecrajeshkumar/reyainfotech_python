c90b9d82ac4dc78213554ccdb115e144ac40783b
Rajesh Kumar <91066031+ecrajeshkumar@users.noreply.github.com>
2026-07-16 00:13:26 +0530

    Basic python programming

---
+++ arithmetic_operator.py
@@ -0,0 +1,19 @@
+
+age1 = 12
+age2 = 15
+age3 = 18
+print("The sum of ages is:", age1 + age2 + age3)
+
+# string concatenation
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+print("Full Name:", full_name)  # Output: Full Name: John Doe
+
+print("Rajesh" * 5)  # Output: RajeshRajeshRajeshRajeshRajesh
+
+sent = "Hello, World!"
+print(sent[0])  # Output: H
+print(sent[7:12])  # Output: World
+
+
---
+++ variable.py
@@ -0,0 +1,24 @@
+# variable and Multiple assignment
+# Variable is a name that refers to a value. It is used to store data in memory
+x = 5
+y = 10
+z = 15  
+print(x, y, z)  # Output: 5 10 15
+sentence = "Hello, World!"
+print(sentence)  # Output: Hello, World!
+
+search, bob, mike = "search", "bob", "mike"
+print(search)  # Output: search
+print(bob)     # Output: bob
+print(mike)    # Output: mike
+
+search = bob = mike = "search"
+print(search)  # Output: search
+print(bob)     # Output: search
+print(mike)    # Output: search
+
+name, age, city = "Alice", 30, "New York"
+print(name)  # Output: Alice
+print(age)   # Output: 30
+print(city)  # Output: New York
+
